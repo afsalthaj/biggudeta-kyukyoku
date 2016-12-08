8a742171adec72fe6bf6c2d1c05bd967e01fc5f8
afsal.thaj <afsal.taj06@gmail.com>
2016-12-08 16:48:09 +1100

    setting up environment

---
+++ configuration-management/ansible/environment/dev.sh
@@ -0,0 +1,5 @@
+export JAVA_HOME=/usr/java/jdk1.7.0_67-cloudera/
+export HADOOP_CONF_DIR=/etc/hadoop/conf
+export SPARK_HOME=/root/spark/spark-2.0.1-bin-hadoop2.7
+
+export PATH=$PATH:$SPARK_HOME/bin:$JAVA_HOME/bin:$HOME/bin
